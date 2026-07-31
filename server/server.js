9432757de3e633bdfdc56558457e242657cb5f18
RAHEL MEY AGATA SITUMORANG <rahelmeiagata@gmail.com>
2026-08-01 03:08:07 +0700

    feat: connect backend to MySQL

--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+import db from "./config/db.js";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    const connection = await db.getConnection();
+
+    console.log("✅ Connected to MySQL");
+
+    connection.release();
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("❌ Gagal koneksi ke MySQL");
+    console.error(err.message);
+  }
+};
+
+startServer();
